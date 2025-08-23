ac99cd7919f11c5742cbb9ca4d99b83e22726e53
L-dog-22 <lutherluxtonwood@gmail.com>
2025-08-23 16:30:02 +0100

    D: added counting func. currently func counts each WORD not each CHAR, also need too add lower func

--- stats.py
+++ stats.py
@@ -1,7 +1,24 @@
+Char_dict = {}
+
+
 with open("/home/l_dog/workspace/github.com/L-dog-22/bookbot/books/frankenstein.txt") as get_book_text:
     file_contents = get_book_text.read(None)
     
 
 word_list = file_contents.split()
 word_count = len(word_list)
-main = print(word_count, "words found in the document")
+for i in word_list:
+    if i in Char_dict:
+        Char_dict[i] += 1
+    else:
+        Char_dict[i] = 1
+
+   
+
+#main = print(word_count, "words found in the document")
+#main = print(stat_dict)
+
+
+cheese = print(word_count, "words found in the document")
+toast = print(Char_dict)
+    
